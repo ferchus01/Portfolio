f8648c7c338455f11d2ed70cbb976ce0e53c9703
ferchus01 <51255607+ferchus01@users.noreply.github.com>
2022-07-25 17:59:30 -0300

    Modifico datos y tecnologias en el home

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -2,13 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillInstagram,
-  AiFillMail,
-} from "react-icons/ai";
-import { 
-  FaLinkedinIn
-} from "react-icons/fa";
+import { AiFillInstagram, AiFillMail } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 export const Home2 = () => {
   return (
@@ -20,18 +15,28 @@ export const Home2 = () => {
               ALGO <span className="purple"> SOBRE MI </span>
             </h1>
             <p className="home-about-body">
-              Me gusta crear soluciones e innovar, de alli que soy programador, y al menos he aprendido algo, creo… 🤷‍♂️
+              Me gusta crear soluciones e innovar, de alli que soy programador,
+              y al menos he aprendido algo, creo… 🤷‍♂️
               <br />
-              <br />En mi día a día suelo usar clásicos como
+              <br />
+              En mi día a día suelo usar clásicos como
               <i>
-                &nbsp;<b className="purple">HTML5, CSS3, Javascript, Reactjs, Angular </b>&nbsp;
-              </i>y otros, con los que me gusta desarrollar apps web.
+                &nbsp;
+                <b className="purple">
+                  HTML5, CSS3, Javascript, Reactjs, Angular, Nodejs, MongoDB{" "}
+                </b>
+                &nbsp;
+              </i>
+              y otros, con los que me gusta desarrollar apps web.
               <br />
               <br />
-                Me interesa aprender sobre los lenguajes y frameworks relacionados a estos temas, así como también reforzar lo que ya he aprendido con el tiempo. &nbsp;
+              Me interesa aprender sobre los lenguajes y frameworks relacionados
+              a estos temas, así como también reforzar lo que ya he aprendido
+              con el tiempo. &nbsp;
               <br />
               <br />
-                Siempre que es posible, realizo cursos e investigo sobre las tecnologias o actualizaciones nuevas que aparecen.
+              Siempre que es posible, realizo cursos e investigo sobre las
+              tecnologias o actualizaciones nuevas que aparecen.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -44,12 +49,13 @@ export const Home2 = () => {
           <Col md={12} className="home-about-social">
             <h1>Contactame</h1>
             <p>
-              Sientete libre de <span className="purple"> conectar </span>conmigo
+              Sientete libre de <span className="purple"> conectar </span>
+              conmigo
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/fernando-ariel-vilca-269791205/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
@@ -82,4 +88,4 @@ export const Home2 = () => {
       </Container>
     </Container>
   );
-}
+};
