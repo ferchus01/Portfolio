a5bb42b2339fbfaf9ef907dad2b2878d7cb79731
ferchus01 <51255607+ferchus01@users.noreply.github.com>
2022-07-25 17:40:07 -0300

    Agrego proyecto journal a la seccion de proyectos

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import { Particle } from "../Particle";
 import gifExpertApp from "../../Assets/Projects/gifExpertApp.PNG";
+import journalApp from "../../Assets/Projects/journalApp.PNG";
 
 export const Projects = () => {
   return (
@@ -10,12 +11,23 @@ export const Projects = () => {
       <Particle />
       <Container>
         <h1 className="project-heading">
-         <strong className="purple">Proyectos </strong>
+          <strong className="purple">Proyectos </strong>
         </h1>
         <p style={{ color: "white" }}>
           Aqui algunos proyectos que realice en este tiempo
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={journalApp}
+              isBlog={false}
+              title="Journal-App"
+              description="Aplicación de notas donde puedes crearte una cuenta y/o loguearte con la misma o con google. Una vez dentro vas a poder
+              crear, editar, eliminar, subir imagenes y ver tus notas. Se uso react, redux, firebase con firestore y autenticación, materialUi, cloudinary, hooks, etc"
+              link="https://journal-app-fer-vilca.netlify.app"
+              repo="https://github.com/ferchus01/journal-app"
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={gifExpertApp}
@@ -30,4 +42,4 @@ export const Projects = () => {
       </Container>
     </Container>
   );
-}
+};
